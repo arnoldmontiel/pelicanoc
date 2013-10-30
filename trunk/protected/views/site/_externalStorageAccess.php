ab04466990ccc6534d1c83b62a324b0ce25f0939
Wensel84@gmail.com <Wensel84@gmail.com@b7154291-7bb7-2336-18e6-1d0484609e77>
2013-10-30 20:31:08 +0000

    PEL-103

--- trunk/protected/views/site/_externalStorageAccess.php
+++ trunk/protected/views/site/_externalStorageAccess.php
@@ -13,3 +13,23 @@
 		echo CHtml::closeTag('p');
 	}
 ?>		
+
+<script>
+
+	$('.usb-button-scan').click(function(){
+		var id = $(this).attr("id");
+		$('#hidden-unit').val(id);
+		$('#hidden-working').val(1);		
+		$.post("<?php echo SiteController::createUrl('AjaxExploreExternalStorage'); ?>",
+			{
+				id:id			    
+			}
+		).success(
+			function(data){	
+				$('#explorer-unit').html(data);							
+		});
+				
+		return false;
+	});
+
+  </script>
